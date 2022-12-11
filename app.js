baf0f2e9a4060d490227588b35ce890332dc1f75
Graciela Ferderer <graciela.ferderer@gmail.com>
2022-12-10 16:35:45 -0800

    added name and pronoun buttons

--- app.js
+++ app.js
@@ -1,3 +1,29 @@
 // grab DOM elements
 
 // set event listeners
+const nameDisplay = document.getElementById('name-display');
+const nameInput = document.getElementById('name-input');
+const button = document.getElementById('update-button');
+button.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    console.log(nameDisplay);
+    // nameDisplay.textContent = 'Julie';
+    nameDisplay.textContent = nameInput.value;
+    nameDisplay.style.color = 'red';
+});
+const pronounDisplay = document.getElementById('pronoun-display');
+const pronounInput = document.getElementById('pronoun-input');
+const updatePb = document.getElementById('update-pb');
+updatePb.addEventListener('click', () => {
+    console.log('i am clicking the button');
+    console.log(pronounDisplay);
+    pronounDisplay.textContent = pronounInput.value;
+    pronounDisplay.style.color = 'black';
+});
+nameDisplay.addEventListener('keydown', () => {
+    console.log('i am typing');
+    console.log(nameDisplay);
+    nameDisplay.textContent = nameInput.value;
+});
+
+
